b453c2e8e921ba69b28ebc288ae145862e80cec6
Deepana Naidu <deepana.naidu@gmail.com>
2022-03-15 15:25:44 +0000

    Added 2 unit tests for Exercise004Test

--- src/test/java/com/techreturners/exercise004/Exercise004Test.java
+++ src/test/java/com/techreturners/exercise004/Exercise004Test.java
@@ -2,11 +2,13 @@ package com.techreturners.exercise004;
 
 import org.junit.Test;
 
+import java.time.DateTimeException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.Month;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class Exercise004Test {
 
@@ -34,7 +36,22 @@ public class Exercise004Test {
     public void checkGetDateTimeWhenBothDateAndTimeIsSpecifiedWithDayRollOver() {
 
         Exercise004 ex004 = new Exercise004(LocalDateTime.of(2021, Month.JANUARY, 24, 23, 59, 59, 0));
-        LocalDateTime expected = LocalDateTime.of(2052, Month.OCTOBER, 03, 1, 46, 39);
+        LocalDateTime expected = LocalDateTime.of(2052, Month.OCTOBER, 3, 1, 46, 39);
+
+        assertEquals(expected, ex004.getDateTime());
+    }
+
+
+    @Test(expected = DateTimeException.class)
+    public void checkGetDateTimeWhenInvalidDateIsEntered() {
+        Exercise004 ex004 = new Exercise004(LocalDate.of(2021, Month.JULY, 90));
+    }
+
+    @Test
+    public void checkGetDateTimeWhenDataTimeIsLeapYear() {
+
+        Exercise004 ex004 = new Exercise004(LocalDateTime.of(2020, Month.FEBRUARY, 29, 23, 59, 59, 0));
+        LocalDateTime expected = LocalDateTime.of(2051, Month.NOVEMBER, 8, 1, 46, 39);
 
         assertEquals(expected, ex004.getDateTime());
     }
