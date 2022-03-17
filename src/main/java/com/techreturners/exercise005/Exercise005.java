8bc535f78cb1f96ce689e91219bc9fe3e801c6ba
Deepana Naidu <deepana.naidu@gmail.com>
2022-03-17 22:12:17 +0000

    Created new constant:ALPHABET_SET

--- src/main/java/com/techreturners/exercise005/Exercise005.java
+++ src/main/java/com/techreturners/exercise005/Exercise005.java
@@ -4,19 +4,15 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Exercise005 {
+    private static final int ALPHABET_SET = 26;
 
     public boolean isPangram(String input) {
         //pangram is a sentence/word using every letter of the alphabet at least once
         Set<Character> set = new HashSet<>();
-
         for(char c : input.toLowerCase().toCharArray()){
             if(Character.isLetter(c)){
                set.add(c);}
-
         }
-
-        return set.size()==26;
-
+        return set.size()==ALPHABET_SET;
     }
-
 }
